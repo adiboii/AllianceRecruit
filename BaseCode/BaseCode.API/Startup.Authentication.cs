670ee3840a5a15f69fb3318ed4ed34d5eb562cce
Kate Aubrey Cellan <cellankateaubrey@gmail.com>
2023-05-07 15:19:48 +0800

    Authentication startup

--- BaseCode/BaseCode.API/Startup.Authentication.cs
+++ BaseCode/BaseCode.API/Startup.Authentication.cs
@@ -14,32 +14,37 @@ namespace BaseCode.API
         private void ConfigureAuth(IServiceCollection services)
         {
             var authSecretKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(Configuration[Constants.Token.SecretKey]));
-            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(options =>
             {
                 options.SaveToken = true;
                 options.TokenValidationParameters = new TokenValidationParameters
                 {
                     ClockSkew = TimeSpan.Zero,              // Clock skew compensates for server time drift.We recommend 5 minutes or less
                     IssuerSigningKey = authSecretKey,       // Specify the key used to sign the token
-                    
+
                     RequireSignedTokens = true,             // Requires that valid tokens should be signed
                     RequireExpirationTime = true,           // Requires that tokens should have expiry date and time
 
                     ValidIssuer = Configuration[Constants.Token.Issuer],           // Validate the JWT Issuer (iss) claim
                     ValidAudience = Configuration[Constants.Token.Audience],       // Validate the JWT Audience (aud) claim
-                    
+
                     ValidateIssuer = true,              // Validate if the token was issued by a trusted authorization server (default true)
                     ValidateIssuerSigningKey = true,    // Validate the token signing key
                     ValidateAudience = true,            // Validate if the token audience matches our audience value (default true)
-                    ValidateLifetime = true,            // Validate the token expiry 
+                    ValidateLifetime = true,            // Validate the token expiry
                 };
             });
         }
 
         private void ConfigureAuth(IApplicationBuilder app)
         {
-            //app.UseMiddleware<TokenProviderMiddleware>();
-            //app.UseMiddleware<RefreshTokenProviderMiddleware>();
+            app.UseMiddleware<TokenProviderMiddleware>();
+            app.UseMiddleware<RefreshTokenProviderMiddleware>();
         }
     }
 }
